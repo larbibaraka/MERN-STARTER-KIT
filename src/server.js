a9e3c8e3f2c0cb2a2223335fd1315ca9063177ae
larbibaraka <laridev@gmail.com>
2019-01-12 16:40:40 +0100

    adding 404

--- src/server.js
+++ src/server.js
@@ -17,6 +17,16 @@ app.use('/api' , Api);
 //serving the static files like css , images ...etc
 app.use(express.static('public'));
 
+
+app.all('*' , (req, res)=>{
+  console.log('Returning a 404 from the catch-all route');
+  return res.render("templates/404");
+})
+
+//error middleware
+//app.use(require('./error-middleware'));
+
+
 app.get('/users' , (req , res)=>{
 
   res.render('index');
@@ -26,6 +36,12 @@ app.get('/users' , (req , res)=>{
 export const start = () =>{
   app.listen(3000, ()=>{
     console.log('server is running on port 3000')
+  })
+}
+
+export const stop = () =>{
+  app.close(3000 , ()=>{
+    console.log(`Shut down on port : ${3000}`);
   })
 }
 
