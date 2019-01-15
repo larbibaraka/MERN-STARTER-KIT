af3656a16639790514bde1942e6f51c7c8e09ed2
larbibaraka <laridev@gmail.com>
2019-01-15 21:40:15 +0100

    adding concurrently

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ class App extends Component {
         <div className="jumbotron jumbotron-fluid">
           <div className="container">
             <h1 className="display-4">{this.props.textValue}</h1>
+            <p>thanks </p>
           </div>
         </div>
       </div>
